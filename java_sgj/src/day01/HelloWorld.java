7072a273acb0a6110cf616adb609a6c5079c7ab5
GyeongZ <sgj00700@naver.com>
2023-12-08 17:15:39 +0900

    자바 1일차 : 안녕 세상아

--- java_sgj/src/day01/HelloWorld.java
+++ java_sgj/src/day01/HelloWorld.java
@@ -3,10 +3,9 @@ package day01;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-
-	System.out.println("Hello world!");
 	
-	System.out.println();
+		System.out.println("Hello world!");
+	
 	}
 
 }
