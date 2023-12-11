1d90349d109af711ec537c37795ff1053c84b5b5
GyeongZ <sgj00700@naver.com>
2023-12-11 18:10:00 +0900

    자바 2일차 : 과제

--- java_sgj/src/day02/HomeworkEx1.java
+++ java_sgj/src/day02/HomeworkEx1.java
@@ -22,13 +22,13 @@ public class HomeworkEx1 {
 			System.out.println("잘못된 점수 입니다.");
 		}else if (score < 60) {
 			System.out.println("F 입니다.");
-		}else if (score >= 60 ||score <= 70) {
+		}else if (score < 70) {
 			System.out.println("D 입니다.");
-		}else if (score > 70 ||score <= 80) {
+		}else if (score < 80) {
 			System.out.println("C 입니다.");
-		}else if (score > 80 ||score <= 90) {
+		}else if (score < 90) {
 			System.out.println("B 입니다.");
-		}else if (score > 90 ||score <= 100) {
+		}else if (score <= 100) {
 			System.out.println("A 입니다.");
 			
 		}
