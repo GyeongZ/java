b377c588d8e338d32e5c0047e1b8576efdea6036
GyeongZ <sgj00700@naver.com>
2023-12-11 18:01:07 +0900

    자바 2일차 : 예제

---
+++ java_sgj/src/day02/IfElseEx1.java
@@ -0,0 +1,17 @@
+package day02;
+
+public class IfElseEx1 {
+
+	public static void main(String[] args) {
+
+			//정수가 0이라면 출력하고, 아니면 0이 아닙니다라고 출력하는 예제
+		int num = 0;
+		
+		if(num == 0) {
+			System.out.println("0입니다.");
+		}else {
+			System.out.println(num);
+		}
+	}
+
+}
---
+++ java_sgj/src/day02/TestEx2.java
@@ -0,0 +1,19 @@
+package day02;
+
+public class TestEx2 {
+	
+	// 홀짝 판별 예제
+	public static void main(String[] args) {
+
+		int num = 3;
+		
+		//num이 짝수이면 짝수라고 출력하고 홀수면 홀수라고 출력하는 코드를 작성하세요.
+		//num를 2로 나누었을 때 나머지가 0과 같으면 짝수라고 출력하고,
+		//num를 2로 나누었을 때 나머지가 0과 같지않으면 홀수라고 출력
+
+		if(num % 2 == 0) {
+			System.out.println(num);
+		}
+	}
+
+}
