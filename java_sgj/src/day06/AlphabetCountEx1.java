9eef4f8d361d6e0670bbffe83546fb37254dbd65
GyeongZ <sgj00700@naver.com>
2023-12-18 18:43:53 +0900

    자바 6일차 : 알파벳 개수 맞추기 예제

--- java_sgj/src/day06/AlphabetCountEx1.java
+++ java_sgj/src/day06/AlphabetCountEx1.java
@@ -29,8 +29,7 @@ public class AlphabetCountEx1 {
 		
 		//강사님
 		// 단어를 입력하게 함 => 문자열을 입력
-		System.out.println("단어를 입력하세요 : ");
-		Scanner scan = new Scanner(System.in);
+		/*System.out.println("단어를 입력하세요 : ");
 		String str = scan.next();
 		int [] arr = new int[26];
 		
@@ -65,6 +64,41 @@ public class AlphabetCountEx1 {
 			ch++;
 		}
 		scan.close();
+		*/		
+		
+		//단어를 입력 => 문자열을 입력
+		System.out.println("단어 입력 : ");
+		Scanner scan = new Scanner(System.in);
+		String str = scan.next();
+	    int [] arr = new int[26];  
+	    //반복문으로 문자열의 길이만큼 반복
+	      for(int i = 0; i< str.length();i++) {
+	         //문자열의 각 문자를 가져옴
+	    	  char ch = str.charAt(i);
+	    	  // 가져온 문자의 배열 번지에 숫자를 증가
+	    	  // 가져온 문자가 a이면 0번지에 있는 숫자를 1증가, b이면 1번지에 있는 숫자를 1증가
+	    	  arr[ch-'a']++;
+	      }
+	      
+	      // 반복문으로 배열의 크기만큼 (26개) 반복
+    	  for(int i=0; i<arr.length; i++) {
+    		  //개수가 0이 아닌 알파벳들을 출력	
+    		  if(arr[i] != 0) {
+	               char ch = (char)('a' + i);
+	               System.out.println(ch + " : " + arr[i]);
+    		  }
+    	  }
+	      System.out.println("===============");
+	      char ch = 'a';
+	      for(int count : arr) {
+	    	  if(count != 0) {
+	    		 System.out.println(ch + ":" + count);
+	    	  }
+	    	  ch++;
+	      }
+	      scan.close();
+	   }
+	
 	}
 
-}
+
