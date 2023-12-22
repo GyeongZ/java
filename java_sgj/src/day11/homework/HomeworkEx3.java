2ef6223da37d26d938158e274d05cf45049e5bdc
GyeongZ <sgj00700@naver.com>
2023-12-22 17:56:27 +0900

    자바 11일차 : 과제

---
+++ java_sgj/src/day11/homework/HomeworkEx3.java
@@ -0,0 +1,16 @@
+package day11.homework;
+
+	public class HomeworkEx3 {
+		//팁! car 클래스 자동차 만들었던것 처럼, 기능 테스트해보는것처럼(ex-깜빡이 켜기 끄기 등)
+	public static void main(String[] args) {
+		/* 사이트에서 회원 관리(로그인, 회원가입 등)를 위한 Member 클래스를 작성하세요.*/
+		
+		//작성한 Member 클래스를 이용하여 인스턴스를 생성하고 구현한 메서드를 테스트해보세요.
+		
+		}
+
+	}
+	
+	class Member{
+	//필요한 멤버변수 및 메서드를 정리하고 구현하세요.
+	}
