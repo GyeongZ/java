12397b47b44d3a637ca7939a99d3a95b4f06726e
GyeongZ <sgj00700@naver.com>
2023-12-27 20:27:26 +0900

    자바 12일차 : 상속 - 메서드 오버라이딩 예제

--- java_sgj/src/day12/OverrideEx1.java
+++ java_sgj/src/day12/OverrideEx1.java
@@ -9,37 +9,47 @@ public class OverrideEx1 {
 		d.print2();
 		System.out.println("--------");
 		d.print1(20);
-
+		}	
 	}
-class C{
-	protected int num1 = 1, num2 = 2;
+	class C{
+		protected int num1 = 1, num2 = 2;
+		
+		protected void print1() {
+			System.out.println(num1);
+			System.out.println(num2);
+		}
 	
-	protected void print1() {
-		System.out.println(num1);
-		System.out.println(num2);
+		public void print2() {
+			System.out.println(num1);
+			System.out.println(num2);
+		}
 	}
-
-	public void print2() {
-		System.out.println(num1);
-		System.out.println(num2);
-	}
-}
-class D extends C{
+	class D extends C{
 	
 
 	@Override
-	
-	/*안되는 이유 1. 매개변수가 부모 클래스에는 없음
-	 * 2. 부모 클래스에서 접근제어자가 protected 였는데 여기서는 default.
-	 
-	void print1(int num1){
-	
-}
-	 */
-	
-}
-{
-	
-}
-}
 
+		
+		/*안되는 이유 1. 매개변수가 부모 클래스에는 없음
+		 * 2. 부모 클래스에서 접근제어자가 protected 였는데 여기서는 default.
+		 
+		void print1(int num1){
+		
+	}
+		 */
+		protected void print1() {
+			System.out.println("num1 : " + num1);
+			System.out.println("num2 : " + num2);
+		}
+		//메서드 오버 로드에 의해 print1 메서드를 추가
+		public void print1(int num1) {
+			System.out.println("num1 : " + num1);
+			System.out.println("num2 : " + num2);
+		}
+		
+		public void print2() {
+			super.print2();
+			System.out.println("B 클래스의 메서드입니다.");
+		}
+		
+	}
