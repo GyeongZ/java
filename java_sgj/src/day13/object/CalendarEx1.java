b64b2154ec5d2e62316a05c0ae11bb90e2b91ac9
GyeongZ <sgj00700@naver.com>
2023-12-27 19:45:22 +0900

    자바 13일차 : 기본 클래스 - Calendar 클래스 예제

---
+++ java_sgj/src/day13/object/CalendarEx1.java
@@ -0,0 +1,19 @@
+package day13.object;
+
+import java.util.Calendar;
+
+public class CalendarEx1 {
+
+	public static void main(String[] args) {
+		//캘린더 예제
+		Calendar cal = Calendar.getInstance();
+		int year = cal.get(Calendar.YEAR);
+		System.out.print(year+"년");
+		int month = cal.get(Calendar.MONTH)+1; //월만 더하기 1 해주기!!!!
+		System.out.print(month+"월");
+		int day = cal.get(Calendar.DAY_OF_MONTH); //Day 종료가 많음
+		System.out.print(day+"일");
+
+	}
+
+}
