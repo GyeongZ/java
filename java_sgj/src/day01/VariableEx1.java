16c581778a46e775401ade6288963c4e65d456b4
GyeongZ <sgj00700@naver.com>
2024-02-29 15:35:38 +0900

    자바 1일차 : 기본형 변수 선언 예제

--- java_sgj/src/day01/VariableEx1.java
+++ java_sgj/src/day01/VariableEx1.java
@@ -11,9 +11,13 @@ public class VariableEx1 {
 		 * 2. 변수를 선언하고 초기화 함
 		 * 변수타입 변수명 = 값,
 		 */
-		char ch1; //문자형 변수 ch1를 선언	
+		
+		//문자형 변수 ch1를 선언
+		char ch1; 	
+		
 		//문자형 변수 ch2를 선언하고, 문자 A로 초기화
 		char ch2 = 'A';
+		
 		//변수는 초기화를 하고, 사용해야 한다.
 		//초기화 되지 않은 ch1을 사용하려고 하면 에러가 발생
 		//System.out.println(ch1);
